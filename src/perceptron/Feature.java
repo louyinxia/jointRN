759fa5b22b928e664763f6d829ff238e2918481f
louyinxia <yinxia@edu.whu.cn>
2016-08-04 12:11:57 +0800

    Update Feature.java

--- src/perceptron/Feature.java
+++ src/perceptron/Feature.java
@@ -8,11 +8,9 @@ public class Feature {
 	public double sum = 0; // The total of weight
 	public int lastUpdateIndex = 0; // lasting index of update
 	public double aveWeight = 0; // average weight
-
 	public enum featureName {
 		CharUnigram, CharBigram;
 	}
-
 	public Feature() {
 	}
 
@@ -24,25 +22,9 @@ public class Feature {
 		this.lastUpdateIndex = index;
 		this.aveWeight = aveWeight;
 	}
-
 	public void AveWeight(int curUpdateIndex) {
 		this.sum += (curUpdateIndex - this.lastUpdateIndex) * this.weight;
 		this.aveWeight = this.sum / curUpdateIndex;
 		this.lastUpdateIndex = curUpdateIndex;
 	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		HashMap<String, Feature> hm = new HashMap<String, Feature>();
-		hm.put("aa", new Feature());
-		Feature f = hm.get("aa");
-		f.sum = 100;
-		System.out.println(hm.get("aa").sum);
-		featureName a = featureName.CharUnigram;
-		System.out.println(a.toString());
-	}
-
 }
